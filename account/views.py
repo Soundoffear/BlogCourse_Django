29d682cc23731694ea8c1d6a88e29c211d188059
soundoffear <m.dyngosz@gmail.com>
2020-10-24 16:07:29 -0500

    Add Account Screen, Login and starting with Bootstrap

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth import login, authenticate, logout
 
-from account.forms import RegistrationForm, AccountAuthenticationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
 
 
 def registration_view(request):
@@ -65,3 +65,30 @@ def login_view(request):
 
     context['login_form'] = form
     return render(request, 'account/login.html', context)
+
+def account_view(request):
+
+    context = {}
+    
+    user = request.user
+
+    if not user.is_authenticated:
+        return redirect("login")
+
+    if request.POST:
+        form = AccountUpdateForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = AccountUpdateForm(
+                initial = {
+                    'email': request.user.email,
+                    'username': request.user.username
+                    }
+                )
+    
+    context['account_form'] = form
+
+    return render(request, 'account/account.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from personal.views import home_screen_view
-from account.views import registration_view, logout_view, login_view
+from account.views import registration_view, logout_view, login_view, account_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
+    path('account/', account_view, name='account'),
 ]
