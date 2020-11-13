1d1f92c129cdb5a2bb042d61cdfae5c1258da103
m.dyngosz@gmail.com <m.dyngosz@gmail.com>
2020-11-13 09:55:27 -0600

    updated home page, login and started working on creating posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from blog.models import BlogPost
 
-# Create your views here.
+def create_blog_view(request):
+
+    context = {}
+
+    return render(request, 'blog/create_post.html', context)
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from personal.views import home_screen_view
 from account.views import registration_view, logout_view, login_view, account_view
 from django.contrib.auth import views as auth_views
@@ -29,7 +29,8 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('login/', login_view, name='login'),
     path('account/', account_view, name='account'),
-    
+    path('blog/', include('blog.urls', 'blog')),
+
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
     path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),name='password_reset_done'),
