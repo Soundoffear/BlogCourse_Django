752bddc849e2f8d44c37449d1d885501726f6f50
m.dyngosz@gmail.com <m.dyngosz@gmail.com>
2020-11-30 14:18:20 -0600

    pagination

--- personal/views.py
+++ personal/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from blog.models import BlogPost
 from operator import attrgetter
 from blog.views import get_blog_queryset
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
+BLOG_POSTS_PER_PAGE = 2
 
 def home_screen_view(request):
     
@@ -10,10 +12,24 @@ def home_screen_view(request):
 
     query = ''
     if request.GET:
-        query = request.GET['q']
+        query = request.GET.get('q', '')
         context['query'] = str(query)
     
     blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
+    
+    # pagitation
+
+    page = request.GET.get('page', 1)
+    blog_post_paginator = Paginator(blog_posts, BLOG_POSTS_PER_PAGE)
+
+    try:
+        blog_posts = blog_post_paginator.page(page)
+    except PageNoteAnInteger:
+        blog_posts = blog_post_paginator.page(BLOG_POSTS_PER_PAGE)
+    except EmptyPage:
+        blog_posts = blog_post_paginator.page(blog_post_paginator.num_pages)
+
     context['blog_posts'] = blog_posts
 
     return render(request, 'personal/home.html', context)
